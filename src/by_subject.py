a02b6623c4fcb85661304ce6abc529bb5ee6b478
Juan Carlos Iglesias <jiglesia@student.42.fr>
2025-04-09 16:36:29 +0200

    train and predict buttons

--- src/by_subject.py
+++ src/by_subject.py
@@ -3,10 +3,24 @@ import src.utils as ut
 from time import sleep
 from src.filter_data import load_data, filter_data
 
+def empty_load_data(subject, task):
+    col_1, col_2 = st.columns(2)
+    col_1.write("Raw data")
+    col_2.write("Filtered data")
+    for raw, fig in load_data(subject, task):
+        with col_1:
+            st.pyplot(fig)
+        with col_2:
+            st.pyplot(filter_data(raw, 13, 30).plot())
+    st.write("Data loaded successfully!")
+
 def train_model():
     st.write("Training model...")
     sleep(5)
     st.write("Model trained successfully!")
+
+def predict():
+    pass
 
 subjects = ut.subjects
 subject = st.selectbox(
@@ -22,23 +36,20 @@ task = st.selectbox(
 )
 
 # files = ut.get_file_name(subject, task)
-is_trained = st.toggle(label="Train model", value=False)
-if is_trained:
+# is_trained = st.toggle(label="Train model", value=False)
+is_trained = f"{subject}_{task}" in st.session_state
+col_1, col_2 = st.columns(2)
+with col_1:
+    train_button = st.button("Train model", disabled=is_trained)
+if train_button:
     placeholder = st.empty()
     with placeholder.container():
-        col_1, col_2 = st.columns(2)
-        col_1.write("Raw data")
-        col_2.write("Filtered data")
-        for raw, fig in load_data(subject, task):
-            with col_1:
-                st.pyplot(fig)
-            with col_2:
-                st.pyplot(filter_data(raw, 13, 30).plot())
-        st.write("Data loaded successfully!")
+        empty_load_data(subject, task)
     with placeholder.container():
-        train_model()
-        st.write("Model trained successfully!")
-    if st.button("Predict"):
-        st.balloons()
-else:
-    st.write("Model not trained")
+        st.session_state[f"{subject}_{task}"] = train_model()
+    st.rerun()
+with col_2:
+    predict_button = st.button("Predict", disabled=not is_trained)
+if predict_button:
+    st.write("Predicting...")
+    predict()
