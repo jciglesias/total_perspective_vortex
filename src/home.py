35b5d69e93dc74d174e1bbf0109f6fc21b821608
Juan Carlos Iglesias <jiglesia@student.42.fr>
2025-04-16 18:10:51 +0200

    save raws and plot them later

---
+++ src/home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.write("# Total Perspective Vortex")
+
+st.write(
+"""\"The Total Perspective Vortex derives its picture of the whole Universe on the principle of
+extrapolated matter analyses. To explain — since every piece of matter in the Universe
+is in some way affected by every other piece of matter in the Universe, it is in theory
+possible to extrapolate the whole of creation — every sun, every planet, their orbits,
+their composition and their economic and social history from, say, one small piece of
+fairy cake. The man who invented the Total Perspective Vortex did so basically in order
+to annoy his wife. Trin Tragula — for that was his name — was a dreamer, a thinker, a
+speculative philosopher or, as his wife would have it, an idiot. And she would nag him
+incessantly about the utterly inordinate amount of time he spent staring out into space,
+or mulling over the mechanics of safety pins, or doing spectrographic analyses of pieces
+of fairy cake. “Have some sense of proportion!” she would say, sometimes as often as
+thirty-eight times in a single day. And so he built the Total Perspective Vortex — just
+to show her. And into one end he plugged the whole of reality as extrapolated from a
+piece of fairy cake, and into the other end he plugged his wife: so that when he turned it
+on she saw in one instant the whole infinity of creation and herself in relation to it. To
+Trin Tragula’s horror, the shock completely annihilated her brain; but to his satisfaction
+he realized that he had proved conclusively that if life is going to exist in a Universe of
+this size, then the one thing it cannot afford to have is a sense of proportion.\"
+Douglas Adams, The Restaurant At The End Of The Universe""")
